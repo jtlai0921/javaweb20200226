95d7278b4e244bd0dccca1cd47642c13f15f6d18
賴紀廷 <49492695+jtlai0921@users.noreply.github.com>
2020-04-08 19:34:01 +0800

    14

---
+++ Web20200408/src/java/model/Product.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author teacher
+ */
+public class Product {
+   private String pid;
+   private String productName;
+   private int price;
+   private String place;
+   public Product(){}
+   public Product(String id , String name,int pr , String pl){
+       pid=id;
+       productName=name;
+       price=pr;
+       place=pl;
+   }
+    /**
+     * @return the pid
+     */
+    public String getPid() {
+        return pid;
+    }
+
+    /**
+     * @param pid the pid to set
+     */
+    public void setPid(String pid) {
+        this.pid = pid;
+    }
+
+    /**
+     * @return the productName
+     */
+    public String getProductName() {
+        return productName;
+    }
+
+    /**
+     * @param productName the productName to set
+     */
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    /**
+     * @return the price
+     */
+    public int getPrice() {
+        return price;
+    }
+
+    /**
+     * @param price the price to set
+     */
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    /**
+     * @return the place
+     */
+    public String getPlace() {
+        return place;
+    }
+
+    /**
+     * @param place the place to set
+     */
+    public void setPlace(String place) {
+        this.place = place;
+    }
+   public String toString(){
+       return pid+"->"+productName+"->"+price +"->"+place;
+   }
+}
