fb2e34119d11d0019e51df56a15d73a5fe1f1744
賴紀廷 <49492695+jtlai0921@users.noreply.github.com>
2020-04-08 20:02:26 +0800

    Update InsertProductServlet.java

--- Web20200408/src/java/InsertProductServlet.java
+++ Web20200408/src/java/InsertProductServlet.java
@@ -37,13 +37,47 @@ public class InsertProductServlet extends HttpServlet {
              String data=request.getParameter("products");
              Gson g=new Gson();
              Product[ ]  pts=g.fromJson(data, Product[ ].class);
-             String msg="";
-             for(Product s : pts)
-                 msg+=s.toString()+"\n";
-             out.println(msg);
+//             String msg="";
+//             for(Product s : pts)
+//                 msg+=s.toString()+"\n";
+//             out.println(msg);
+             boolean f=insert(pts);
+             if(f)
+                 out.println( "多筆資料新增成功");
+             else
+                 out.println( "多筆資料新增失敗");
         }
     }
-
+    public boolean insert(Product[ ]  pts){
+        boolean flag=true;
+         String url = "jdbc:mysql://localhost:3306/classicmodels?serverTimezone=CST";
+        String username = "root";
+        String password = "1234";
+        String sql="";
+        try{
+             Class.forName("com.mysql.cj.jdbc.Driver");
+             Connection con =  DriverManager.getConnection("jdbc:mysql://localhost:3306/goods?serverTimezone=CST","root","1234");
+      
+             con.setAutoCommit(false);    
+             sql="insert into goods.product(pid,productName,price ,place) values(?,?,?,?)";
+             for(Product pt : pts){
+             PreparedStatement insert = con.prepareStatement(sql);        
+                insert.setString(1, pt.getPid()); 
+                insert.setString(2, pt.getProductName()); 
+                insert.setInt(3, pt.getPrice());
+                insert.setString(4, pt.getPlace());           
+                insert.executeUpdate();
+             }
+            flag=true;
+            con.commit();
+              con.setAutoCommit(true);    
+        }catch(Exception ex){
+            System.out.println("DB Error:"+ex.getMessage());
+            flag=false;
+        }
+        
+        return flag;
+    }   
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
